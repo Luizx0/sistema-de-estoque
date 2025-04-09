8cc9ffdd508a9a94abce96d362350833dc38763e
Luizx0 <Loue0982@gmail.com>
2025-04-09 17:24:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    const loginContainer = document.querySelector('.login-container');
+    const dashboardContainer = document.querySelector('.dashboard-container');
+    const productForm = document.getElementById('product-form');
+    const productList = document.getElementById('product-list');
+
+   
+    const allowedEmails = ['us1@exemplo.com', 'us2@exemplo.com', 'adm@exemplo.com']; // Email Fictício
+
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+
+        if (allowedEmails.includes(email)) {
+   
+            if (password === 'senha123') {  // Senha fictícia
+                loginContainer.style.display = 'none';
+                dashboardContainer.style.display = 'block';
+            } else {
+                alert('Senha inválida!');
+            }
+        } else {
+            alert('E-mail não autorizado!');
+        }
+    });
+
+
+    productForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const productName = document.getElementById('product-name').value;
+        const productBrand = document.getElementById('product-brand').value;
+        const productSize = document.getElementById('product-size').value;
+        const productQuantity = document.getElementById('product-quantity').value;
+        const productPrice = document.getElementById('product-price').value;
+        const productColor = document.getElementById('product-color').value;
+
+        const newProductRow = document.createElement('tr');
+        newProductRow.innerHTML = `
+            <td>${productName}</td>
+            <td>${productBrand}</td>
+            <td>${productSize}</td>
+            <td>${productQuantity}</td>
+            <td>${productPrice}</td>
+            <td>${productColor}</td>
+            <td><button>Editar</button></td>
+        `;
+        productList.appendChild(newProductRow);
+
+
+        productForm.reset();
+    });
+});
